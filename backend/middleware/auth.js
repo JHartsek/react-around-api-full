474c4c4950c8eb4cbaf155a56398f57db932ba3c
Julia <JLHartsek@gmail.com>
2022-10-04 22:02:10 -0400

    remove error handling from auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,16 +1,17 @@
+const { UnauthorizedError } = require('../errors/UnauthorizedError');
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Authorization failed' });
+    throw new UnauthorizedError('Authorization failed');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, 'encoding-string');
   } catch (err) {
-    return res.status(403).send({ message: 'Unauthorized action!' });
+    throw new UnauthorizedError('Authorization failed');
   }
   req.user = payload;
   next();
